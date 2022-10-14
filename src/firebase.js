e573ff13c1ae9f180e2e01ca7e26f01615a69c7b
yyyljy <yyyljy@gmail.com>
2022-10-14 23:14:32 +0900

    first

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDe2CgbFsi4HbbArUzfEwLx151tqKm0fw",
+  authDomain: "twitterclone-c18b1.firebaseapp.com",
+  projectId: "twitterclone-c18b1",
+  storageBucket: "twitterclone-c18b1.appspot.com",
+  messagingSenderId: "72792558265",
+  appId: "1:72792558265:web:794187ba79764c5047e968",
+};
+
+export default initializeApp;
