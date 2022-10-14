2df9a01a1d5007c4c8139d2f85604313eb886ee2
yyyljy <yyyljy@gmail.com>
2022-10-14 23:26:20 +0900

    hide keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBDe2CgbFsi4HbbArUzfEwLx151tqKm0fw",
-  authDomain: "twitterclone-c18b1.firebaseapp.com",
-  projectId: "twitterclone-c18b1",
-  storageBucket: "twitterclone-c18b1.appspot.com",
-  messagingSenderId: "72792558265",
-  appId: "1:72792558265:web:794187ba79764c5047e968",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default initializeApp;
